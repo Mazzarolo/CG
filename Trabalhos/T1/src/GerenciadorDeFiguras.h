30634725162c0bf340e61df9e813713f2b6888e8
João Mazzarolo <jdrmazzarolo@outlook.com>
2023-03-30 16:08:39 -0300

    Starting figire list class

---
+++ Trabalhos/T1/src/GerenciadorDeFiguras.h
@@ -0,0 +1,15 @@
+#ifndef GERENCIADORDEFIGURAS_H
+#define GERENCIADORDEFIGURAS_H
+
+#include "Figura.h"
+
+using namespace std;
+
+class GerenciadorDeFiguras
+{
+private:
+
+};
+
+
+#endif // GERENCIADORDEFIGURAS_H
